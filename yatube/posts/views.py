85223d9ddfcc6685a02db01548e783c84a04f5e2
Anastasiya Cherkasova <cherkasovaam@bk.ru>
2023-05-01 18:49:45 +0300

    ready to rev2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post, Group, Follow, Comment
+from .models import Post, Group, Follow
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from .forms import PostForm, CommentForm
