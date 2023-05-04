835f5150702cacf03e73351ac7e9665b0b7a1f6d
Anastasiya Cherkasova <cherkasovaam@bk.ru>
2023-05-04 12:31:37 +0300

    after rev

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -35,6 +35,11 @@ ALLOWED_HOSTS = [
     'testserver',
 ]
 
+# Добавьте IP адреса, при обращении с которых будет доступен DjDT
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -50,6 +55,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
